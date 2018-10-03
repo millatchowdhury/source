714f693e6cd6708400cadd8dbeea5066f4e5a752
millatchowdhury <millatchowdhury@gmail.com>
2018-10-03 09:29:44 +0600

    something

---
+++ test.php
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+<?php
+
+
+?>
+
+
+
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+    <input type="file" name="name">
+    <input type="submit" value="Submit">
+</form>
